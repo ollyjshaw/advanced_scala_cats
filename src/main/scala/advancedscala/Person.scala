72b45bacbf90100545ce150ccd629d0acfda7a8b
Olly Shaw <ollyjshaw@gmail.com>
2017-04-02 12:09:33 +0100

    extension methods

--- src/main/scala/advancedscala/Person.scala
+++ src/main/scala/advancedscala/Person.scala
@@ -2,6 +2,12 @@ package advancedscala
 
 final case class Person(name: String, email: String)
 
+object JsonSyntax {
+  implicit class JsonWriterOps[A](value: A) {
+    def toJson(implicit w: JsonWriter[A]): Json = w.write(value)
+  }
+}
+
 object JsonWriterInstances {
   implicit val stringJsonWriter = new JsonWriter[String] {
     def write(value: String): Json = JsString(value)
--- src/test/scala/advancedscala/PersonTest.scala
+++ src/test/scala/advancedscala/PersonTest.scala
@@ -1,6 +1,7 @@
 package advancedscala
 import org.scalatest.{Matchers, FlatSpec}
 import JsonWriterInstances._
+import JsonSyntax._
 
 class PersonTest extends FlatSpec with Matchers{
 
@@ -8,6 +9,15 @@ class PersonTest extends FlatSpec with Matchers{
     val dave = Person("Dave", "dave@dave.com")
     val david_json = Json.toJson(dave)
     println(david_json)
+    david_json.asInstanceOf[JsObject].get.size shouldBe 2
+    david_json.asInstanceOf[JsObject].get.size shouldBe 2
+  }
+
+  "A Person" should "serializable to Json with extension method" in {
+    val dave = Person("Dave", "dave@dave.com")
+    val david_json = dave.toJson
+    println(david_json)
+    david_json.asInstanceOf[JsObject].get.size shouldBe 2
     david_json.asInstanceOf[JsObject].get.size shouldBe 2
   }
 
